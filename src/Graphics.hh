79161bce4e1ee051fbe0a82710e2e806a798f28b
N. Pattakos <pattakosn@fastmail.com>
2021-01-12 12:59:08 +0100

    moved OpenGL staff to its own class

---
+++ src/Graphics.hh
@@ -0,0 +1,10 @@
+#ifndef TRIAN_GRAPHICS_HH
+#define TRIAN_GRAPHICS_HH
+class Graphics {
+ public:
+  Graphics();
+  ~Graphics();
+  void Background(float, float, float);
+  void loop(void);
+};
+#endif  // TRIAN_GRAPHICS_HH
