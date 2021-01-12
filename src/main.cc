ffbac30b24000d72d78e0929b1f0f5cd58a92adb
N. Pattakos <pattakosn@fastmail.com>
2021-01-12 12:59:08 +0100

    moved OpenGL staff to its own class

--- src/main.cc
+++ src/main.cc
@@ -1,15 +1,9 @@
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-
 #include <cstring>
 #include <iostream>
 #include <memory>
 
+#include "Graphics.hh"
 #include "Mesh.hh"
-GLFWwindow *window;
-#include <glm/glm.hpp>
-
-using namespace glm;
 
 static int poolsize = 1024;
 static bool DataFileUse = false;
@@ -37,64 +31,6 @@ static void ArgsCLIParse(int argc, char **argv) {
 }
 
 int main(int argc, char **argv) {
-  // Initialise GLFW
-  if (!glfwInit()) {
-    fprintf(stderr, "Failed to initialize GLFW\n");
-    getchar();
-    return -1;
-  }
-
-  glfwWindowHint(GLFW_SAMPLES, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT,
-                 GL_TRUE);  // To make MacOS happy; should not be needed
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-  // Open a window and create its OpenGL context
-  window = glfwCreateWindow(1024, 768, "Tutorial 01", nullptr, nullptr);
-  if (window == nullptr) {
-    fprintf(stderr,
-            "Failed to open GLFW window. If you have an Intel GPU, they are "
-            "not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
-    getchar();
-    glfwTerminate();
-    return -1;
-  }
-  glfwMakeContextCurrent(window);
-
-  // Initialize GLEW
-  if (glewInit() != GLEW_OK) {
-    fprintf(stderr, "Failed to initialize GLEW\n");
-    getchar();
-    glfwTerminate();
-    return -1;
-  }
-
-  // Ensure we can capture the escape key being pressed below
-  glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-
-  // Dark blue background
-  glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
-
-  do {
-    // Clear the screen. It's not mentioned before Tutorial 02, but it can cause
-    // flickering, so it's there nonetheless.
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    // Draw nothing, see you in tutorial 2 !
-
-    // Swap buffers
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-
-  }  // Check if the ESC key was pressed or the window was closed
-  while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
-         glfwWindowShouldClose(window) == 0);
-
-  // Close OpenGL window and terminate GLFW
-  glfwTerminate();
-
   std::cout << "arguments given:\n";
   ArgsCLIShow(argc, argv);
   ArgsCLIParse(argc, argv);
@@ -117,6 +53,10 @@ int main(int argc, char **argv) {
     MyMesh = std::make_unique<Mesh>(poolsize);
   }
   MyMesh->PrintFirstN();
+
+  Graphics visualize;
+  visualize.Background(0.0f, 0.0f, 0.4f);  // Dark blue background
+  visualize.loop();
 
   //  Mesh *aMesh = new Mesh(1024, false);
   //  fprintf(stdout, "New Mesh Created\nAs long as a [%d] vertex pool", 1024);
