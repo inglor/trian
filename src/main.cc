46658e063480446b11590ca6cd1a297a775e1547
N. Pattakos <pattakosn@fastmail.com>
2021-01-12 12:59:48 +0100

    delete commented out useless code in main

--- src/main.cc
+++ src/main.cc
@@ -58,22 +58,5 @@ int main(int argc, char **argv) {
   visualize.Background(0.0f, 0.0f, 0.4f);  // Dark blue background
   visualize.loop();
 
-  //  Mesh *aMesh = new Mesh(1024, false);
-  //  fprintf(stdout, "New Mesh Created\nAs long as a [%d] vertex pool", 1024);
-
-  //  fprintf(stdout, "\nParta.... \n");
-  //  aMesh->Trian_Mesh_ShowPool();
-
-  //  FILE * _dataset;
-  //  _dataset = fopen ( "coords.bin" , "wb" );
-  //  if (_dataset)
-  //  {
-  //    aMesh->Trian_Mesh_Save_Dataset(_dataset);
-  //    fclose(_dataset);
-  //    fprintf(stdout, "File saved...\n");
-  //  }
-  //
-  //  delete aMesh;
-  //  fprintf(stdout, "\n");
   return 0;
 }
