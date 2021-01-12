c8a5c482af907ea638e2bacb6450d69e68fccfbc
N. Pattakos <pattakosn@fastmail.com>
2021-01-12 12:20:18 +0100

    main automated hook fix

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,9 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
 #include "Trian.hh"
-
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
 GLFWwindow *window;
 #include <glm/glm.hpp>
 
@@ -21,7 +21,7 @@ int main(int argc, char **argv) {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT,
-                 GL_TRUE); // To make MacOS happy; should not be needed
+                 GL_TRUE);  // To make MacOS happy; should not be needed
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   // Open a window and create its OpenGL context
@@ -61,7 +61,7 @@ int main(int argc, char **argv) {
     glfwSwapBuffers(window);
     glfwPollEvents();
 
-  } // Check if the ESC key was pressed or the window was closed
+  }  // Check if the ESC key was pressed or the window was closed
   while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
          glfwWindowShouldClose(window) == 0);
 
