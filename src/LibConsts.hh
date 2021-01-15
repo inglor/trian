b73a53a0131ce6bb395cc66610bbb2da4baa08b4
Leonidas Spyropoulos <artafinde@gmail.com>
2021-01-15 09:59:03 +0000

    Extract constants to a separate .hh file and let linker inline them
    Signed-off-by: Leonidas Spyropoulos <artafinde@gmail.com>

---
+++ src/LibConsts.hh
@@ -0,0 +1,7 @@
+#pragma once
+
+static inline float const MAX_VERTEX_FLOAT = 10.0f;
+static inline float const MIN_VERTEX_FLOAT = -10.0f;
+static inline int const COORD_X_INDEX = 0;
+static inline int const COORD_Y_INDEX = 1;
+static inline int const COORD_Z_INDEX = 2;
--- src/Mesh.hh
+++ src/Mesh.hh
@@ -2,14 +2,9 @@
 
 #include <vector>
 
+#include "LibConsts.hh"
 #include "Triangle.hh"
 #include "Vertex.hh"
-
-static const float MAX_VERTEX_FLOAT = 10.0f;
-static const float MIN_VERTEX_FLOAT = -10.0f;
-static const int COORD_X_INDEX = 0;
-static const int COORD_Y_INDEX = 1;
-static const int COORD_Z_INDEX = 2;
 
 const auto compare_x = [](const Vertex &v1, const Vertex &v2) {
   return v1[COORD_X_INDEX] < v2[COORD_X_INDEX];
